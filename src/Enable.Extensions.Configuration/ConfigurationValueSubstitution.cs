368fd1bc012946d20cb958df34b552cf5dbf025c
Matt Sharpe <mdsharpe@users.noreply.github.com>
2023-07-07 16:00:39 +0100

    Substitute service bus shared access key from environment variable (#3)
    * Lowercase machine name
    * Substitute service bus shared access key
    
    ---------
    
    Co-authored-by: Matthew Sharpe <matthew.sharpe@enable.com>

--- src/Enable.Extensions.Configuration/ConfigurationValueSubstitution.cs
+++ src/Enable.Extensions.Configuration/ConfigurationValueSubstitution.cs
@@ -6,17 +6,20 @@ internal class ConfigurationValueSubstitution
     private readonly string _branchPath;
     private readonly string _databaseServer;
     private readonly string _machineName;
+    private readonly string _serviceBusSharedAccessKey;
 
     public ConfigurationValueSubstitution(
         string branch,
         string branchPath,
         string databaseServer,
-        string machineName)
+        string machineName,
+        string serviceBusSharedAccessKey)
     {
         _branch = branch.Replace(' ', '_');
         _branchPath = branchPath.Replace(' ', '_');
         _databaseServer = databaseServer.Replace(' ', '_');
         _machineName = machineName.Replace(' ', '_');
+        _serviceBusSharedAccessKey = serviceBusSharedAccessKey;
     }
 
     public string? Substitute(string settingValue)
@@ -30,6 +33,7 @@ internal class ConfigurationValueSubstitution
             .Replace("{Branch}", _branch)
             .Replace("{BranchPath}", _branchPath)
             .Replace("{DatabaseServer}", _databaseServer)
-            .Replace("{MachineName}", _machineName);
+            .Replace("{MachineName}", _machineName)
+            .Replace("{ServiceBusSharedAccessKey}", _serviceBusSharedAccessKey);
     }
 }
