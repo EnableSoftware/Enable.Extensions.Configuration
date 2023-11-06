a905485550bc08b93cb162aa028a1a81965780dc
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-06 11:15:55 +0000

    Add test console app. Update NuGet dependencies.

---
+++ test/Enable.Extensions.Configuration.TestConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Enable.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+configuration.ApplyDevelopmentOverrides();
+
+Console.WriteLine(configuration["AllPlaceholders"]);
