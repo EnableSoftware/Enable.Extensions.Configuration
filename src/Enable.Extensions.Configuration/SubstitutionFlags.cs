94687c041db3409b4b48ee9d5e97d241dec2447c
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-06 11:54:52 +0000

    Enum values fix

--- src/Enable.Extensions.Configuration/SubstitutionFlags.cs
+++ src/Enable.Extensions.Configuration/SubstitutionFlags.cs
@@ -4,7 +4,7 @@
 public enum SubstitutionFlags
 {
     None = 0,                         // 00000000
-    TextFiles = 1 << 1,               // 00000001
-    EnvironmentVariables = 1 << 2,    // 00000010
-    ThrowIfTextFileNotFound = 1 << 3, // 00000100
+    TextFiles = 1 << 0,               // 00000001
+    EnvironmentVariables = 1 << 1,    // 00000010
+    ThrowIfTextFileNotFound = 1 << 2, // 00000100
 }
